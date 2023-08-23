0762026c8898245135a6865683345f824444526a
Juan Lambre <juanmlambre@gmail.com>
2023-08-23 10:31:40 -0300

    Clean up stuff

--- src/devServer/endpoints/bypassRequest.ts
+++ src/devServer/endpoints/bypassRequest.ts
@@ -8,22 +8,16 @@ export async function bypassRequest(req: Request, res: Response) {
 
   const form = new FormData();
   Object.entries(req.body).forEach(([key, value]) => {
-    console.log(">>> APPEND", key, value);
     form.set(key, value.toString());
   });
 
   const opts = {
     method: req.method,
     body: form,
-    headers: {
-      // "Content-Type": req.headers["content-type"],
-      cookie: req.headers.cookie,
-    },
+    headers: { cookie: req.headers.cookie },
   };
 
   const response = await fetch(url, opts);
-
-  console.log(">>> OPTS", JSON.stringify(opts));
 
   const result = await response.json();
   console.log(">>> RESULT", JSON.stringify(result));
