86800f855a4a2adbad4388a84737f6f957115494
Juan Lambre <juanmlambre@gmail.com>
2023-08-25 10:26:52 -0300

    Redirect home button

--- src/scratch/modals/LostModal.ts
+++ src/scratch/modals/LostModal.ts
@@ -1,18 +1,10 @@
 import $ from "jquery";
+import { Backend } from "../../backend/Backend";
 
 export namespace LostModal {
   let modalElement: HTMLElement;
   let usedElement: HTMLElement;
   let expiredElement: HTMLElement;
-
-  function init() {
-    modalElement = $("#modal-nogano")[0];
-    usedElement = $("#modal-nogano #txtYaUtilizadaNoGano")[0];
-    expiredElement = $("#modal-nogano #txtTiempoFuera")[0];
-
-    $("#modal-nogano #btnCerrar").on("click", hide);
-    $("#modal-nogano #btnChance").on("click", hide);
-  }
 
   export function setUsed() {
     usedElement.style.display = "initial";
@@ -28,6 +20,21 @@ export namespace LostModal {
 
   export function hide() {
     modalElement.style.display = "none";
+  }
+
+  // "Private" functions
+
+  function init() {
+    modalElement = $("#modal-nogano")[0];
+    usedElement = $("#modal-nogano #txtYaUtilizadaNoGano")[0];
+    expiredElement = $("#modal-nogano #txtTiempoFuera")[0];
+
+    $("#modal-nogano #btnCerrar").on("click", hide);
+    $("#modal-nogano #btnChance").on("click", redirectHome);
+  }
+
+  function redirectHome() {
+    location.href = Backend.config.host + Backend.config.endpoints.home;
   }
 
   init();
--- src/scratch/modals/WinModal.ts
+++ src/scratch/modals/WinModal.ts
@@ -1,14 +1,8 @@
 import $ from "jquery";
+import { Backend } from "../../backend/Backend";
 
 export namespace WinModal {
   let modalElement: HTMLElement;
-
-  function init() {
-    modalElement = $("#modal-gano")[0];
-
-    $("#modal-gano #btnCerrar").on("click", hide);
-    $("#modal-gano #btnChance").on("click", hide);
-  }
 
   export function setPrize() {
     // TODO: Set modal prize
@@ -24,6 +18,19 @@ export namespace WinModal {
 
   export function hide() {
     modalElement.style.display = "none";
+  }
+
+  // "Private" functions
+
+  function init() {
+    modalElement = $("#modal-gano")[0];
+
+    $("#modal-gano #btnCerrar").on("click", hide);
+    $("#modal-gano #btnChance").on("click", redirectHome);
+  }
+
+  function redirectHome() {
+    location.href = Backend.config.host + Backend.config.endpoints.home;
   }
 
   init();
